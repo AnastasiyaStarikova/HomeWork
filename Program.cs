106565fecb083a3df3046713c14a06f48e803e30
AnastasiyaStarikova <127649386+AnastasiyaStarikova@users.noreply.github.com>
2023-03-12 20:34:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your name");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name.ToUpper()}");
