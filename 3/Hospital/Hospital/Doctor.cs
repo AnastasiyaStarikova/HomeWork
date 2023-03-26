686718eedadc744d051882f358f2a5a7351da428
AnastasiyaStarikova <127649386+AnastasiyaStarikova@users.noreply.github.com>
2023-03-26 18:57:33 +0700

    Add files via upload

---
+++ 3/Hospital/Hospital/Doctor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    public abstract class Doctor : OnlineHospital
+    {
+        public new string Name { get; set; }
+        public string Speciality { get; set; }
+        public int Experiense { get; set; }
+        public int Age { get; set; }
+        public virtual void Display()
+        {
+            Console.WriteLine($" ФИО врача - {Name} : возраст - {Age}, стаж - {Experiense}, специальность - {Speciality}");
+
+        }
+    }
+     public class Terapy : Doctor
+    {
+        public bool WorksInHospital { get; set; }
+
+        public override void Display ()
+        {
+            base.Display();
+            if (WorksInHospital == true)
+            {
+                Console.WriteLine("Терапевт работает в online больнице");
+            }
+        }
+    }
+    public class Neurology : Doctor
+    {
+        public bool WorksInHospital { get; set; }
+
+        public override void Display()
+        {
+            base.Display();
+            if (WorksInHospital == true)
+            {
+                Console.WriteLine("Невролог работает в online больнице");
+            }
+        }
+    }
+    public class Ophtalmy : Doctor
+    {
+        public bool WorksInHospital { get; set; }
+
+        public override void Display()
+        {
+            base.Display();
+            if (WorksInHospital == true)
+            {
+                Console.WriteLine("Офтальмолог работает в online больнице");
+            }
+        }
+    } 
+
+}
---
+++ 3/Hospital/Hospital/OnlineHospital.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    public class OnlineHospital
+    {
+        public string Name { get; set; }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($" {Name} ");
+        }
+    }
+}
