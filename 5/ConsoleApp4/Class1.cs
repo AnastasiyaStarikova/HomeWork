81c1b43d384f46e5fa9735531f449fd8bb4b89c1
AnastasiyaStarikova <127649386+AnastasiyaStarikova@users.noreply.github.com>
2023-04-10 09:46:44 +0700

    Add files via upload

---
+++ 5/ConsoleApp4/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+
+    {
+        public class FileRecord
+        {
+            public string Title { get; set; }
+            public string Type { get; set; }
+            public string Path { get; set; }
+            public DateTime LastChanges { get; set; }
+        }
+    }
+}
